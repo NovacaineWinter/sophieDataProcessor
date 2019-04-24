4e244f1865e64e59aec1ed7c9e98b3de5a76eb69
Matt Hartley <matt.v.hartley@gmail.com>
2019-04-24 16:01:10 +0100

    data structures explained

--- dataManipulation.py
+++ dataManipulation.py
@@ -13,19 +13,81 @@ data = csv.reader(f)
 i=0
 
 for row in data:
-	if i < 10:			# i only want to show the first 10 lines rather than thousands
-		print 'Data in row '+str(i)		# print out i, but we have to tell python it is a string, otherwise it will try to perform an addition 
+	if i < 0:			# i only want to show the first 10 lines rather than thousands
+	
 		print row		#print out the data that we have in row
-		print ' '
-		print 'First field: '+ row[0]
-		print 'Second field: '+ row[1]
-		print 'Third field: '+ row[2]		#note the zero index !
-		print 'Fourth field: '+ row[3]
-		print 'Fifth field: '+ row[4]
-		print 'Sixth field: '+ row[5]
-		print 'Seventh field: '+ row[5]
-		print 'End for data for row '+str(i)
-		print ' '
-		print ' '
+		
 	i+=1				#add 1 on to i for each time through the loop
 
+"""
+the variable "row" is a list, lists can be accessed by their positions, which is what we did in the previous example, 
+	for instance, we access the first position with row[0], the second by row[1] and so on
+
+	instead of lists, we can use "dictionaries" which is pythons way of saying giving things names
+"""
+
+my_list = {}	#empty dict
+
+my_list['sport'] = 'climbing'
+my_list['favouritePet'] = 'dog'
+my_list['age'] = 28		#can also have integers in
+my_list['dogs'] = ['Monte','Benson']  # can also be a list
+
+print " "		#get us some space in the terminal
+print " "
+print " "
+
+print 'my favourite sport is ' + my_list['sport']
+print " "
+
+print "I am "+str( my_list['age'] ) + ' years old';
+print " "
+"""
+print my_list
+
+
+
+#	Dicts are a good way of keeping like data together - the key names are very very useful - we will 
+#	exploit this with the names of your rice. other than the names, they behave exactly like their variable types
+
+	
+#	we can do more interesting things like this;
+
+
+print " "
+print "I have "+ str(  len(  my_list['dogs']  )  ) + ' dogs'
+
+
+
+#even more cool, we can loop over a list when we know how long it is, using the len() function
+
+
+namesOfMyDogs = ''   # empty string
+
+for i in range(0,len(my_list['dogs'])):
+
+	namesOfMyDogs+= my_list['dogs'][i]+', '
+
+#we can now say...
+
+print 'Their names are '+namesOfMyDogs
+
+
+
+
+
+##	we can then add some other items to a list, even after we have done other stuff with it 
+
+
+my_list['dogs'].append('jerry')
+my_list['dogs'].append('Zebedee')
+
+namesOfAllDogs = ''   # empty string
+
+for i in range(0,len(my_list['dogs'])):
+
+	namesOfAllDogs+= my_list['dogs'][i]+', '
+
+
+print "but when we go "+ my_list['sport']+" we often have "+str(len(my_list['dogs']))+" dogs. All together we have "+namesOfAllDogs
+"""
