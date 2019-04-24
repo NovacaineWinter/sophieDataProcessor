16043272b9958d3b040fb48030c8f16ca4d26d53
Matt Hartley <matt.v.hartley@gmail.com>
2019-04-24 14:59:03 +0100

    example of how to access the data in each of the row positions

--- dataManipulation.py
+++ dataManipulation.py
@@ -14,6 +14,18 @@ i=0
 
 for row in data:
 	if i < 10:			# i only want to show the first 10 lines rather than thousands
-		print row
+		print 'Data in row '+str(i)		# print out i, but we have to tell python it is a string, otherwise it will try to perform an addition 
+		print row		#print out the data that we have in row
+		print ' '
+		print 'First field: '+ row[0]
+		print 'Second field: '+ row[1]
+		print 'Third field: '+ row[2]		#note the zero index !
+		print 'Fourth field: '+ row[3]
+		print 'Fifth field: '+ row[4]
+		print 'Sixth field: '+ row[5]
+		print 'Seventh field: '+ row[5]
+		print 'End for data for row '+str(i)
+		print ' '
+		print ' '
 	i+=1				#add 1 on to i for each time through the loop
 
