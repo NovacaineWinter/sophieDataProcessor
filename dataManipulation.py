d2794785f028ebcad6974f8fb5a108865cf95b20
Matt Hartley <matt.v.hartley@gmail.com>
2019-04-26 17:16:08 +0100

    Completed Script

--- dataManipulation.py
+++ dataManipulation.py
@@ -1,93 +1,151 @@
 import csv
 import sys
 
+
+def getUniqueRiceLines(data):
+	#loop through to get all the names of the rice into one big long list
+	allRice = []
+	for i in range(0,len(data)):
+		allRice.append(data[i][2])
+
+	uniqueRice = list(set(allRice))			#set is a data structure that cannot have duplicates in, turn it into a set then turn it back into a list
+
+	return uniqueRice
+
+
+
+def parseDataInput(data):
+	allData = []
+	for row in data:
+		allData.append(row);
+
+	return allData
+
+
+def isNum(input):
+	try: 
+		float(input)
+		return True
+	except:
+		return False
+
+
 # sys.argv[1] is the file location passed over command line
 	#open reads the file passed into the var f. 
 	#'rU' is read Universal. read as opposed to write, Universal is allowing for maximum compatibility with each csv format
 f = open(sys.argv[1], 'rU')
 
+
 #pass the file to the csv reader 
 data = csv.reader(f)
 
-#initialise a counter
-i=0
 
-for row in data:
-	if i < 0:			# i only want to show the first 10 lines rather than thousands
+#this is the percentile for choosing the data
+print "Enter percentile to filter by as an integer i.e 95% as 95"
+percentile = float(float(int(input()))/100);
+
+allData = parseDataInput(data)
+
+uniqueRice = getUniqueRiceLines(allData)
+
+
+
+
+
+print 'Detected ' + str(len(uniqueRice)) +' rice types'
+
+
+"""
+	Create the places we are going to store data 
+"""
+
+dataStore = {} # create a place to store the data
+maxVals = {}	#here we will store the maximum gs values for each rice in order to work out 95%
+dataOutput = {}
+
+for i in range(0,len(uniqueRice)):
+	dataStore[uniqueRice[i]] = [[],[],[],[],[],[]] 	#create an empty list called by the name of the rice 
 	
-		print row		#print out the data that we have in row
-		
-	i+=1				#add 1 on to i for each time through the loop
-
-"""
-the variable "row" is a list, lists can be accessed by their positions, which is what we did in the previous example, 
-	for instance, we access the first position with row[0], the second by row[1] and so on
-
-	instead of lists, we can use "dictionaries" which is pythons way of saying giving things names
-"""
-
-my_list = {}	#empty dict
-
-my_list['sport'] = 'climbing'
-my_list['favouritePet'] = 'dog'
-my_list['age'] = 28		#can also have integers in
-my_list['dogs'] = ['Monte','Benson']  # can also be a list
-
-print " "		#get us some space in the terminal
-print " "
-print " "
-
-print 'my favourite sport is ' + my_list['sport']
-print " "
-
-print "I am "+str( my_list['age'] ) + ' years old';
-print " "
-"""
-print my_list
-
-
-
-#	Dicts are a good way of keeping like data together - the key names are very very useful - we will 
-#	exploit this with the names of your rice. other than the names, they behave exactly like their variable types
-
+	dataOutput[uniqueRice[i]] = [[],[],[],[],[],[]] 	#create an empty list called by the name of the rice 
 	
-#	we can do more interesting things like this;
-
-
-print " "
-print "I have "+ str(  len(  my_list['dogs']  )  ) + ' dogs'
-
-
-
-#even more cool, we can loop over a list when we know how long it is, using the len() function
-
-
-namesOfMyDogs = ''   # empty string
-
-for i in range(0,len(my_list['dogs'])):
-
-	namesOfMyDogs+= my_list['dogs'][i]+', '
-
-#we can now say...
-
-print 'Their names are '+namesOfMyDogs
+	maxVals[uniqueRice[i]] = [0,0,0,0,0,0] 	#create an zero var for storing the max gs values for each  
 
 
 
 
 
-##	we can then add some other items to a list, even after we have done other stuff with it 
+#insert the data points 
+for i in range(1,len(allData)):
+	dataindex = (int(allData[i][3])-1)
+	dataStore[allData[i][2]][int(allData[i][3])-1].append(allData[i])
 
 
-my_list['dogs'].append('jerry')
-my_list['dogs'].append('Zebedee')
 
-namesOfAllDogs = ''   # empty string
+for i in range(0,len(uniqueRice)):
 
-for i in range(0,len(my_list['dogs'])):
+	thisRice = uniqueRice[i]		#goes through each rice in turn
+	for k in range(0,6):		
+		numDataPoints = len(dataStore[thisRice][k])
+		for j in range(0,numDataPoints):
 
-	namesOfAllDogs+= my_list['dogs'][i]+', '
+			if(isNum(dataStore[thisRice][k][j][5])):
+				if( float(dataStore[thisRice][k][j][5]) > maxVals[thisRice][k] ):
+					#print 'new high found for '
+					maxVals[thisRice][k] = float(dataStore[thisRice][k][j][5])
+
+		dataStore[thisRice][k] = sorted(dataStore[thisRice][k], key=lambda k: float(k[1]))
 
 
-print "but when we go "+ my_list['sport']+" we often have "+str(len(my_list['dogs']))+" dogs. All together we have "+namesOfAllDogs
-"""
+
+
+#we now have the data segmented up by rice, and we have the maximum gs values for each
+#lets go through again and find out which ones are below 95%
+
+for i in range(0,len(uniqueRice)):
+
+
+	thisRice = uniqueRice[i]		#goes through each rice in turn
+	for k in range(0,6):
+
+		carryingOn = True;
+
+		for j in range(0,len(dataStore[thisRice][k])):
+
+			if(isNum(dataStore[thisRice][k][j][5])):
+
+				if(float(dataStore[thisRice][k][j][5]) < (float(maxVals[thisRice][k] * percentile)) and carryingOn):
+
+					dataOutput[thisRice][k].append( dataStore[thisRice][k][j])
+
+				else:
+					carryingOn = False;
+			else:
+				
+				dataOutput[thisRice][k].append( dataStore[thisRice][k][j])
+
+
+	#print str(len(dataStore[thisRice]))+' data points in rice '+ thisRice +', max val:'+str(maxVals[thisRice])
+
+
+
+toCSV = []
+
+for i in range(0,len(uniqueRice)):
+
+	thisRice = uniqueRice[i]
+
+	for k in range(0,6):
+
+		for j in range(0,len(dataOutput[thisRice][k])):
+			toCSV.append(dataOutput[thisRice][k][j])
+
+print str(len(allData))+' data points imported '+str(len(toCSV)) +' data points exported to export.csv, '+str(len(allData) - len(toCSV))+' removed'
+
+with open('export.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(toCSV)
+
+csvFile.close()
+
+
+
